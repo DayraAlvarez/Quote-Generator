68ec762174a526422ca0679b962d8803e2339006
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-07-11 00:21:22 -0500

    Alert added

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ console.log(data);
 
 var iceBreaker =Object.keys(data);
 console.log(iceBreaker);
+
+document.getElementById("button").addEventListener("click", function(){
+    swal("The woods are lovely, dark and deep; but i have promises to keep and miles to go before i sleep");
+});
