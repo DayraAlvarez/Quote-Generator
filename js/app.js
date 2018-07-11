6373e9544c2af31e33dbb879b5a609fc39aa7409
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-07-10 23:49:56 -0500

    First version

---
+++ js/app.js
@@ -0,0 +1,4 @@
+console.log(data);
+
+var iceBreaker =Object.keys(data);
+console.log(iceBreaker);
---
+++ js/data.js
@@ -0,0 +1,4 @@
+var data = {
+    quote1:"It's never to late to be who you might have been",
+    author:"Lewis Carrol"
+};
