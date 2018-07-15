f9698f70b90c6490163485eaac550bd91c43d656
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-07-14 20:20:15 -0500

    Adding design

--- js/data.js
+++ js/data.js
@@ -2,3 +2,11 @@ var data = {
     quote1:"It's never to late to be who you might have been",
     author:"Lewis Carrol"
 };
+
+"Andabamos sin buscarnos pero sabiendo que andabamos para encontrarnos"
+author:"Julio Cortazar"
+
+"The woods are lovely, dark and deep; but i have promises to keep and miles to go before i sleep"
+author:"Robert Frost"
+
+""
